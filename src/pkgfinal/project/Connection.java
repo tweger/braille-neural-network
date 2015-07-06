505db91aeb4e5185d8934bd7d493ed921282beb5
tweger <laamo17@gmail.com>
2015-07-06 15:51:47 -0700

    upload files

---
+++ src/pkgfinal/project/Connection.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgfinal.project;
+
+/**
+ *
+ * @author Pumbadumba
+ */
+public class Connection {
+    
+}
---
+++ src/pkgfinal/project/Neuron.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgfinal.project;
+
+/**
+ *
+ * @author Pumbadumba
+ */
+public class Neuron {
+    boolean fired;
+    int[] weights;
+}
